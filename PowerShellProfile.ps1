94f70b7dbcff956df839e1a88a91ca846221f3fe
ch0p57ickz <jrprompanya@ucla.edu>
2015-02-25 11:16:45 -0800

    Initial checkin of some random scripts in my collection

---
+++ PowerShellProfile.ps1
@@ -0,0 +1,51 @@
+﻿$gitHubPath = Join-Path $env:LOCALAPPDATA "GitHub"
+
+$poshGitRoot = Get-ChildItem -Path $gitHubPath -Filter "Posh*"
+$portableGitRoot = Get-ChildItem -Path $gitHubPath -Filter "PortableGit*"
+
+$rubyFolder = Get-ChildItem -Path "$env:SystemDrive\" -Filter "Ruby*" | Sort-Object -Property "Name" -Descending | Select-Object -First 1
+$pythonFolder = Get-ChildItem -Path "$env:SystemDrive\" -Filter "Python*" | Sort-Object -Property "Name" -Descending | Select-Object -First 1
+
+function Set-Path
+{
+param($path)
+
+    if(($env:Path -split ';') -inotcontains $path)
+    {
+        $env:Path = "$($env:Path.TrimEnd(';'));$path"
+    }
+}
+
+Set-Path (Join-Path ($rubyFolder.FullName) "bin")
+Set-Path $pythonFolder.FullName
+Set-Path (Join-Path ($portableGitRoot.FullName) "bin")
+
+Push-Location $poshGitRoot.FullName
+
+# Load posh-git module from current directory
+Import-Module .\posh-git
+
+# If module is installed in a default location ($env:PSModulePath),
+# use this instead (see about_Modules for more information):
+# Import-Module posh-git
+
+# Set up a simple prompt, adding the git prompt parts inside git repos
+function global:prompt {
+    $realLASTEXITCODE = $LASTEXITCODE
+
+    # Reset color, which can be messed up by Enable-GitColors
+    $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
+
+    Write-Host($pwd.ProviderPath) -nonewline
+
+    Write-VcsStatus
+
+    $global:LASTEXITCODE = $realLASTEXITCODE
+    return "> "
+}
+
+Enable-GitColors
+
+Pop-Location
+
+Start-SshAgent -Quiet
---
+++ Save-BitlockerRecoverykeys.ps1
@@ -0,0 +1,38 @@
+﻿param(
+    [Parameter()]
+    [String[]]
+    $Computers = (
+        "japrom-alkari",
+        "japrom-beast"
+        )
+)
+
+$code = {
+
+$possiblePaths = @("C:\Users\japrom\SkyDrive Pro\BitLockerKeys", "C:\Users\japrom\OneDrive for Business\BitLockerKeys")
+
+$DestinationRoot = $null
+foreach($path in $possiblePaths)
+{
+    if (Test-Path $path)
+    {
+        $DestinationRoot = $path
+        break
+    }
+}
+
+Import-Module Bitlocker -erroraction SilentlyContinue
+(Get-BitLockerVolume).KeyProtector | `
+    Where-Object { $_.KeyProtectorType -eq "RecoveryPassword" } | `
+    foreach { 
+        $keyProtector = $_.KeyProtectorId.Trim("{", "}")
+        echo ("{0} {1} ({2})" -f $keyProtector,$_.RecoveryPassword,$env:COMPUTERNAME) 
+        $filePath = Join-Path $DestinationRoot ("{0} ({1}).txt" -f $keyProtector,$env:COMPUTERNAME)
+        # echo $filePath
+        Set-Content -Path $filePath -Value $_.RecoveryPassword
+    }
+}
+
+Invoke-Command -ComputerName $Computers -ScriptBlock $code #-Authentication Credssp -Credential (Get-Credential)
+# Must be admin to pssession your own pc
+#$code.Invoke()
