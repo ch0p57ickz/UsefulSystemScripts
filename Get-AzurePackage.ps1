3a778e017a1358890cda88b5166acf3a94d39c10
ch0p57ickz <jrprompanya@ucla.edu>
2015-05-28 16:13:20 -0700

    AzurePackage and IsDebug
    - AzurePackage makes it easy to get the cspkg from a deployed azure
    service (Azure APIs did not already exist when i wrote this, for some
    reason)
    - Show-IsDebug determines if a dll is built in release or debug mode (or
    at least tries to. there are apparently nuances here)

---
+++ Get-AzurePackage.ps1
@@ -0,0 +1,19 @@
+﻿param(
+    $Certificate = (Get-Item Cert:\CurrentUser\My\B831688A0548243560527E7284B18AA1E8D241AE),
+    $SubscriptionId = "b538190b-a51d-4c88-b13e-dde7899be912",
+    $CloudServiceName = "icehealthmon-loadperftest",
+    [ValidateSet("production", "staging")]
+    $DeploymentSlot = "production",
+    $ContainerUri = "https://icehealthmonloadperf.blob.core.windows.net/test"
+)
+
+$ServiceUrl = "https://management.core.windows.net/$($SubscriptionId)/services/hostedservices/$CloudServiceName/deploymentslots/$DeploymentSlot/package"
+$ServiceUrl += "?containerUri=$ContainerUri"
+
+$Headers = @{
+    "x-ms-version" = "2014-10-01"
+}
+
+$ServiceUrl
+
+Invoke-WebRequest -Uri $ServiceUrl -Headers $Headers -Certificate $Certificate -Method Post
---
+++ Show-IsDebug.ps1
@@ -0,0 +1,33 @@
+﻿param(
+[Parameter(Mandatory)]
+    $Assembly
+)
+$debugAttr = [System.Reflection.Assembly]::LoadFile($Assembly).GetCustomAttributes([System.Diagnostics.DebuggableAttribute], $false) | select-object -first 1
+
+$buildType = "Release"
+if ($debugAttr)
+{
+    $debug = ([System.Diagnostics.DebuggableAttribute]$debugAttr)
+    $hasDebuggableAttribute = $true
+    $isJitOptimized = ($debug.IsJITTrackingEnabled)
+
+    if ($isJitOptimized)
+    {
+        $buildType = "Debug"
+    }
+
+    $debugModes = [System.Diagnostics.DebuggableAttribute+DebuggingModes]
+    $DebugOutput = "pdb-only"
+    if (($debug.DebuggingFlags -band $debugModes::Default) -ne [int]$debugModes::None)
+    {
+        $DebugOutput = "Full"
+    }
+    
+}
+else
+{
+    $isJitOptimized = $true
+}
+
+
+$isJitOptimized,$buildType,$DebugOutput
